3a9eff93b5957267ef34d8098cb24ad6974a608e
Hyp <otlichnik56@yandex.ru>
2022-11-30 21:18:55 +0500

    first commit for feature otlichnik56

--- telegram-bot-command/src/main/java/pro/sky/telegrambot/TelegramBotApplication.java
+++ telegram-bot-command/src/main/java/pro/sky/telegrambot/TelegramBotApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class TelegramBotApplication {
 
+	// feature otlichnik56
 	public static void main(String[] args) {
 		SpringApplication.run(TelegramBotApplication.class, args);
 	}
