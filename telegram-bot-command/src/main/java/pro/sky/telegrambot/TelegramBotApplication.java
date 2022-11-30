c6c84b5ff67c348765f5a50774750143fbaad915
Hyp <otlichnik56@yandex.ru>
2022-11-30 21:07:17 +0500

    first commit for develop

--- telegram-bot-command/src/main/java/pro/sky/telegrambot/TelegramBotApplication.java
+++ telegram-bot-command/src/main/java/pro/sky/telegrambot/TelegramBotApplication.java
@@ -8,6 +8,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableScheduling
 public class TelegramBotApplication {
 
+	// develop
 	public static void main(String[] args) {
 		SpringApplication.run(TelegramBotApplication.class, args);
 	}
