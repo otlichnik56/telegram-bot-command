824e4dac8f101c8c38ed9a4b5039ec481a850922
Hyp <otlichnik56@yandex.ru>
2022-12-12 23:31:51 +0700

    Пробую сделать вывод

--- telegram-bot-command/src/main/java/pro/sky/telegrambot/Buttons/InlineKeyboards.java
+++ telegram-bot-command/src/main/java/pro/sky/telegrambot/Buttons/InlineKeyboards.java
@@ -1,34 +1,89 @@
 package pro.sky.telegrambot.Buttons;
 
-import org.telegram.telegrambots.api.objects.replykeyboard.InlineKeyboardMarkup;
-import org.telegram.telegrambots.api.objects.replykeyboard.buttons.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardButton;
+import com.pengrad.telegrambot.model.request.InlineKeyboardMarkup;
+import com.pengrad.telegrambot.model.request.Keyboard;
+import pro.sky.telegrambot.constants.Constants;
 
-import java.util.ArrayList;
-import java.util.List;
-
+// не используются. не получилось подключить
 public class InlineKeyboards {
 
-    public void generate(){
-
-        List<List<InlineKeyboardButton>> buttonsMain = new ArrayList<>();
-
-        List<InlineKeyboardButton> buttonShelterInformation = new ArrayList<>();
-        buttonShelterInformation.add(new InlineKeyboardButton().setText("Информация о приюте").setCallbackData("CallFi4a"));
-        buttonsMain.add(buttonShelterInformation);
-
-        List<InlineKeyboardButton> buttonAdoptDog = new ArrayList<>();
-        buttonAdoptDog.add(new InlineKeyboardButton().setText("приютить собаку").setCallbackData("CallFi4a"));
-        buttonsMain.add(buttonAdoptDog);
-
-        List<InlineKeyboardButton> buttonSubmitReport = new ArrayList<>();
-        buttonSubmitReport.add(new InlineKeyboardButton().setText("Прислать отчет о питомце").setCallbackData("CallFi4a"));
-        buttonsMain.add(buttonSubmitReport);
-
-        List<InlineKeyboardButton> buttonCallVolunteer = new ArrayList<>();
-        buttonCallVolunteer.add(new InlineKeyboardButton().setText("Позвать волонтера").setCallbackData("CallFi4a"));
-        buttonsMain.add(buttonCallVolunteer);
-
-        InlineKeyboardMarkup inlineKeyboardMarkup = new InlineKeyboardMarkup();
-        inlineKeyboardMarkup.setKeyboard(buttonsMain);
+    public Keyboard generateMainKeyboard(){
+        return new InlineKeyboardMarkup(
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton(Constants.KEYBOARD_MAIM_SHELTER_INFORMATION).callbackData("/information")
+                },
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton(Constants.KEYBOARD_MAIM_ADOPT_DOG).callbackData(Constants.KEYBOARD_MAIM_ADOPT_DOG)
+                },
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton(Constants.KEYBOARD_MAIM_SUBMIT_REPORT).callbackData(Constants.KEYBOARD_MAIM_SUBMIT_REPORT)
+                },
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton(Constants.KEYBOARD_CALL_VOLUNTEER).callbackData(Constants.KEYBOARD_CALL_VOLUNTEER)
+                });
     }
+
+    public Keyboard generateOneKeyboard(){
+        return new InlineKeyboardMarkup(
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton(Constants.KEYBOARD_ONE_SHELTER_INFORMATION).callbackData(Constants.KEYBOARD_ONE_SHELTER_INFORMATION)
+                },
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton(Constants.KEYBOARD_ONE_WORK_SCHEDULE).callbackData(Constants.KEYBOARD_ONE_WORK_SCHEDULE)
+                },
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton(Constants.KEYBOARD_ONE_ACCIDENT_PREVENTION).callbackData(Constants.KEYBOARD_ONE_ACCIDENT_PREVENTION)
+                },
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton(Constants.KEYBOARD_CONTACT).callbackData(Constants.KEYBOARD_CONTACT)
+                },
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton(Constants.KEYBOARD_CALL_VOLUNTEER).callbackData(Constants.KEYBOARD_CALL_VOLUNTEER)
+                },
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton(Constants.KEYBOARD_MAIN_MENU).callbackData(Constants.KEYBOARD_MAIN_MENU)
+                });
+    }
+
+    public Keyboard generateTwoKeyboard(){
+        return new InlineKeyboardMarkup(
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton(Constants.KEYBOARD_TWO_DATING_DOG).callbackData(Constants.KEYBOARD_TWO_DATING_DOG)
+                },
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton(Constants.KEYBOARD_TWO_DOCUMENTS_ADOPT_DOG).callbackData(Constants.KEYBOARD_TWO_DOCUMENTS_ADOPT_DOG)
+                },
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton(Constants.KEYBOARD_TWO_TRANSPORTATION_DOG).callbackData(Constants.KEYBOARD_TWO_TRANSPORTATION_DOG)
+                },
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton(Constants.KEYBOARD_TWO_SMALL_DOG).callbackData(Constants.KEYBOARD_TWO_SMALL_DOG)
+                },
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton(Constants.KEYBOARD_TWO_BIG_DOG).callbackData(Constants.KEYBOARD_TWO_BIG_DOG)
+                },
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton(Constants.KEYBOARD_TWO_INVALID_DOG).callbackData(Constants.KEYBOARD_TWO_INVALID_DOG)
+                },
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton(Constants.KEYBOARD_TWO_CYNOLOGIST_ADVICE).callbackData(Constants.KEYBOARD_TWO_CYNOLOGIST_ADVICE)
+                },
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton(Constants.KEYBOARD_TWO_GOOD_CYNOLOGIST).callbackData(Constants.KEYBOARD_TWO_GOOD_CYNOLOGIST)
+                },
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton(Constants.KEYBOARD_TWO_NOT_DOG).callbackData(Constants.KEYBOARD_TWO_NOT_DOG)
+                },
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton(Constants.KEYBOARD_CONTACT).callbackData(Constants.KEYBOARD_CONTACT)
+                },
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton(Constants.KEYBOARD_CALL_VOLUNTEER).callbackData(Constants.KEYBOARD_CALL_VOLUNTEER)
+                },
+                new InlineKeyboardButton[]{
+                        new InlineKeyboardButton(Constants.KEYBOARD_MAIN_MENU).callbackData(Constants.KEYBOARD_MAIN_MENU)
+                });
+    }
+
 }
