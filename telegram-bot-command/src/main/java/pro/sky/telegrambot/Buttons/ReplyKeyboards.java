a74bae3fbf24f3233c6cb6c9b1db3f118d99d419
VMishutkin <vladmish@list.ru>
2022-12-14 16:46:57 +0700

    Пробую сделать вывод

--- telegram-bot-command/src/main/java/pro/sky/telegrambot/Buttons/ReplyKeyboards.java
+++ telegram-bot-command/src/main/java/pro/sky/telegrambot/Buttons/ReplyKeyboards.java
@@ -1,54 +1,80 @@
 package pro.sky.telegrambot.Buttons;
 
 import com.pengrad.telegrambot.model.request.Keyboard;
+import com.pengrad.telegrambot.model.request.KeyboardButton;
 import com.pengrad.telegrambot.model.request.ReplyKeyboardMarkup;
 import pro.sky.telegrambot.constants.Constants;
 
+import static pro.sky.telegrambot.constants.Constants.*;
+
+
+// кнопки, которые отображаются из главного меню, этапа 1 и 2. генерирует три разные клавиатуры
 public class ReplyKeyboards {
 
-
     public Keyboard generateMainKeyboard(){
-        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(Constants.KEYBOARD_MAIM_SHELTER_INFORMATION,
-                Constants.KEYBOARD_MAIM_ADOPT_DOG ,
-                Constants.KEYBOARD_MAIM_SUBMIT_REPORT,
-                Constants.KEYBOARD_CALL_VOLUNTEER);
-        replyKeyboardMarkup.resizeKeyboard(true); //подгоняем размер
-        replyKeyboardMarkup.oneTimeKeyboard(true); //скрываем после использования
-        Keyboard keyboard = replyKeyboardMarkup;
-        return keyboard;
+
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(
+                new KeyboardButton[]{
+                        new KeyboardButton(Constants.KEYBOARD_MAIM_SHELTER_INFORMATION),
+                        new KeyboardButton(Constants.KEYBOARD_MAIM_ADOPT_DOG)
+                },
+                new KeyboardButton[]{
+                        new KeyboardButton(Constants.KEYBOARD_MAIM_SUBMIT_REPORT),
+                        new KeyboardButton(Constants.KEYBOARD_CALL_VOLUNTEER)
+                });
+        replyKeyboardMarkup.resizeKeyboard(true);
+        replyKeyboardMarkup.oneTimeKeyboard(true);
+        return replyKeyboardMarkup;
     }
 
     public Keyboard generateOneKeyboard(){
-        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(Constants.KEYBOARD_ONE_SHELTER_INFORMATION,
-                Constants.KEYBOARD_ONE_WORK_SCHEDULE ,
-                Constants.KEYBOARD_ONE_ACCIDENT_PREVENTION,
-                Constants.KEYBOARD_CONTACT,
-                Constants.KEYBOARD_CALL_VOLUNTEER);
-        replyKeyboardMarkup.resizeKeyboard(true); //подгоняем размер
-        replyKeyboardMarkup.oneTimeKeyboard(true); //скрываем после использования
-        Keyboard keyboard = replyKeyboardMarkup;
-        return keyboard;
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(
+                new KeyboardButton[]{
+                        new KeyboardButton(KEYBOARD_ONE_SHELTER_INFORMATION),
+                        new KeyboardButton(KEYBOARD_ONE_WORK_SCHEDULE)
+                },
+                new KeyboardButton[]{
+                        new KeyboardButton(KEYBOARD_ONE_ACCIDENT_PREVENTION),
+                        new KeyboardButton(KEYBOARD_CONTACT)
+                },
+                new KeyboardButton[]{
+                        new KeyboardButton(KEYBOARD_CALL_VOLUNTEER),
+                        new KeyboardButton("Главное меню")
+                });
+        replyKeyboardMarkup.resizeKeyboard(true);
+        replyKeyboardMarkup.oneTimeKeyboard(true);
+        return replyKeyboardMarkup;
     }
 
     public Keyboard generateTwoKeyboard(){
-        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(Constants.KEYBOARD_TWO_DATING_DOG,
-                Constants.KEYBOARD_TWO_DOCUMENTS_ADOPT_DOG ,
-                Constants.KEYBOARD_TWO_TRANSPORTATION_DOG,
-                Constants.KEYBOARD_TWO_SMALL_DOG,
-                Constants.KEYBOARD_TWO_BIG_DOG,
-                Constants.KEYBOARD_TWO_INVALID_DOG,
-                Constants.KEYBOARD_TWO_CYNOLOGIST_ADVICE,
-                Constants.KEYBOARD_TWO_GOOD_CYNOLOGIST,
-                Constants.KEYBOARD_TWO_NOT_DOG,
-                Constants.KEYBOARD_CONTACT,
-                Constants.KEYBOARD_CALL_VOLUNTEER);
-        replyKeyboardMarkup.resizeKeyboard(true); //подгоняем размер
-        replyKeyboardMarkup.oneTimeKeyboard(true); //скрываем после использования
-        Keyboard keyboard = replyKeyboardMarkup;
-        return keyboard;
+        ReplyKeyboardMarkup replyKeyboardMarkup = new ReplyKeyboardMarkup(
+                new KeyboardButton[]{
+                        new KeyboardButton(KEYBOARD_TWO_DATING_DOG),
+                        new KeyboardButton(KEYBOARD_TWO_DOCUMENTS_ADOPT_DOG)
+                },
+                new KeyboardButton[]{
+                        new KeyboardButton(KEYBOARD_TWO_TRANSPORTATION_DOG),
+                        new KeyboardButton(KEYBOARD_TWO_SMALL_DOG)
+                },
+                new KeyboardButton[]{
+                        new KeyboardButton(KEYBOARD_TWO_BIG_DOG),
+                        new KeyboardButton(KEYBOARD_TWO_INVALID_DOG)
+                },
+                new KeyboardButton[]{
+                        new KeyboardButton(KEYBOARD_TWO_CYNOLOGIST_ADVICE),
+                        new KeyboardButton(KEYBOARD_TWO_GOOD_CYNOLOGIST)
+                },
+                new KeyboardButton[]{
+                        new KeyboardButton(KEYBOARD_TWO_NOT_DOG),
+                        new KeyboardButton(KEYBOARD_CONTACT)
+                },
+                new KeyboardButton[]{
+                        new KeyboardButton(KEYBOARD_CALL_VOLUNTEER),
+                        new KeyboardButton("Главное меню")
+                });
+        replyKeyboardMarkup.resizeKeyboard(true);
+        replyKeyboardMarkup.oneTimeKeyboard(true);
+        return replyKeyboardMarkup;
     }
-
-
-
 
 }
--- telegram-bot-command/src/main/java/pro/sky/telegrambot/service/TelegramBotUpdatesListener.java
+++ telegram-bot-command/src/main/java/pro/sky/telegrambot/service/TelegramBotUpdatesListener.java
@@ -3,28 +3,32 @@ package pro.sky.telegrambot.service;
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.Keyboard;
 import com.pengrad.telegrambot.request.SendMessage;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import pro.sky.telegrambot.Buttons.InlineKeyboards;
 import pro.sky.telegrambot.Buttons.ReplyKeyboards;
-import pro.sky.telegrambot.constants.Constants;
-
 
 import javax.annotation.PostConstruct;
 import java.util.List;
+
+import static pro.sky.telegrambot.constants.Constants.*;
 
 @Service
 public class TelegramBotUpdatesListener implements UpdatesListener {
 
     private Logger logger = LoggerFactory.getLogger(TelegramBotUpdatesListener.class);
-    private final ReplyKeyboards replyKeyboards = new ReplyKeyboards();
-    private final ShelterService shelterService;
-
-    public TelegramBotUpdatesListener(ShelterService shelterService) {
-        this.shelterService = shelterService;
-    }
+    private final static Keyboard replyMainKeyboards = new ReplyKeyboards().generateMainKeyboard();
+    private final static Keyboard replyOneKeyboards = new ReplyKeyboards().generateOneKeyboard();
+    private final static Keyboard replyTwoKeyboards = new ReplyKeyboards().generateTwoKeyboard();
+    private final static Keyboard inlineMainKeyboards = new InlineKeyboards().generateMainKeyboard();
+    private final static Keyboard inlineOneKeyboards = new InlineKeyboards().generateOneKeyboard();
+    private final static Keyboard inlineTwoKeyboards = new InlineKeyboards().generateTwoKeyboard();
+    private Boolean recordStatus = false;
+    private final static Long volunteerChatId = -1001816802535L;
 
     @Autowired
     private TelegramBot telegramBot;
@@ -43,43 +47,126 @@ public class TelegramBotUpdatesListener implements UpdatesListener {
         return UpdatesListener.CONFIRMED_UPDATES_ALL;
     }
 
-    private void messageProcessing(Update update){
-        long chatId = update.message().chat().id();
-        switch (update.message().text()) {
-            case "/start":
-                SendMessage messageMain = new SendMessage(chatId, Constants.WELCOME_MESSAGE_MAIN);
-                messageMain.replyMarkup(replyKeyboards.generateMainKeyboard());
-                telegramBot.execute(messageMain);
-                break;
-            case Constants.KEYBOARD_MAIM_SHELTER_INFORMATION:
-                SendMessage messageOne = new SendMessage(chatId, Constants.WELCOME_MESSAGE_ONE);
-                messageOne.replyMarkup(replyKeyboards.generateOneKeyboard());
-                telegramBot.execute(messageOne);
-                break;
-            case Constants.KEYBOARD_MAIM_ADOPT_DOG:
-                SendMessage messageTwo = new SendMessage(chatId, Constants.WELCOME_MESSAGE_TWO);
-                messageTwo.replyMarkup(replyKeyboards.generateTwoKeyboard());
-                telegramBot.execute(messageTwo);
-                break;
-            case Constants.KEYBOARD_MAIM_SUBMIT_REPORT:
-                SendMessage messageThree = new SendMessage(chatId, Constants.WELCOME_MESSAGE_THREE);
-                telegramBot.execute(messageThree);
-                break;
-            case Constants.KEYBOARD_CALL_VOLUNTEER:
-                SendMessage messageFour = new SendMessage(chatId, Constants.WELCOME_MESSAGE_FOUR);
-                telegramBot.execute(messageFour);
-                break;
-            case "/hello":
-                SendMessage messageN = new SendMessage(chatId, shelterService.greetings());
-                telegramBot.execute(messageN);
-                break;
-
-                // СЮДА НАПИСАТЬ ДЕЙСТВИЯ НА ОСТАВШИЕСЯ КНОПКИ
-
-            default:
-                SendMessage message = new SendMessage(chatId, Constants.SORRY_MESSAGE);
+    private void messageProcessing(Update update) {
+        Long chatId = getChatId(update);
+        if (recordStatus) {
+            if (!(update.message() == null)) {
+                String text = "Ребят, новый контакт, запишите  " + update.message().text() + " " + update.message().chat().firstName() + " " + update.message().chat().lastName();
+                SendMessage message = new SendMessage(volunteerChatId, text);
                 telegramBot.execute(message);
+                recordStatus = false;
+            }
+        } else {
+            if (!(update.message() == null)) {
+                telegramBot.execute(createMessage(chatId, update.message().text()));
+                if (!(update.message().text() == null) && update.message().text().equals(KEYBOARD_CALL_VOLUNTEER)){
+                    SendMessage messageVolunteer = new SendMessage(volunteerChatId, MESSAGE_FOR_VOLUNTEER + " " + "@" + update.message().from().username());
+                    telegramBot.execute(messageVolunteer);
+                }
+            }
         }
     }
+
+    private Long getChatId(Update update) {
+        Long chatId = null;
+        try {
+            if (update.message() == null) {
+                if (update.callbackQuery() == null) {
+                    chatId = update.myChatMember().chat().id();
+                } else {
+                    chatId = update.callbackQuery().message().chat().id();
+                }
+            } else {
+                chatId = update.message().chat().id();
+            }
+        } catch (NullPointerException notInitializedChatId) {
+            System.out.println("Сработал блок try catch в методе getChatId() " + chatId);
+        }
+        return chatId;
+    }
+
+    private SendMessage createMessage(Long chatId, String text) {
+        SendMessage message;
+        System.out.println(text);
+        if(text == null){
+            message = new SendMessage(chatId, SORRY_MESSAGE);
+            return message;
+        };
+
+        switch (text) {
+            // главное меню и общее
+            case "null":
+            case "/start":
+            case "Главное меню":
+                message = new SendMessage(chatId, WELCOME_MESSAGE_MAIN);
+                message.replyMarkup(replyMainKeyboards);
+                break;
+            case KEYBOARD_MAIM_SHELTER_INFORMATION:
+                message = new SendMessage(chatId, WELCOME_MESSAGE_ONE);
+                message.replyMarkup(replyOneKeyboards);
+                break;
+            case KEYBOARD_MAIM_ADOPT_DOG:
+                message = new SendMessage(chatId, WELCOME_MESSAGE_TWO);
+                message.replyMarkup(replyTwoKeyboards);
+                break;
+            case KEYBOARD_MAIM_SUBMIT_REPORT:
+                message = new SendMessage(chatId, WELCOME_MESSAGE_THREE);
+                recordStatus = true;
+                break;
+            case KEYBOARD_CALL_VOLUNTEER:
+                message = new SendMessage(chatId, WELCOME_MESSAGE_FOUR);
+                break;
+            case KEYBOARD_CONTACT:
+                message = new SendMessage(chatId, RECORD_CONTACT);
+                recordStatus = true;
+                break;
+            // меню один
+            case KEYBOARD_ONE_SHELTER_INFORMATION:
+                message = new SendMessage(chatId, "заглушка");
+                break;
+            case KEYBOARD_ONE_WORK_SCHEDULE:
+                message = new SendMessage(chatId, "заглушка");
+                break;
+            case KEYBOARD_ONE_ACCIDENT_PREVENTION:
+                message = new SendMessage(chatId, "заглушка");
+                break;
+            // меню два
+            case KEYBOARD_TWO_DATING_DOG:
+                message = new SendMessage(chatId, "заглушка");
+                break;
+            case KEYBOARD_TWO_DOCUMENTS_ADOPT_DOG:
+                message = new SendMessage(chatId, "заглушка");
+                break;
+            case KEYBOARD_TWO_TRANSPORTATION_DOG:
+                message = new SendMessage(chatId, "заглушка");
+                break;
+            case KEYBOARD_TWO_SMALL_DOG:
+                message = new SendMessage(chatId, "заглушка");
+                break;
+            case KEYBOARD_TWO_BIG_DOG:
+                message = new SendMessage(chatId, "заглушка");
+                break;
+            case KEYBOARD_TWO_INVALID_DOG:
+                message = new SendMessage(chatId, "заглушка");
+                break;
+            case KEYBOARD_TWO_CYNOLOGIST_ADVICE:
+                message = new SendMessage(chatId, "заглушка");
+                break;
+            case KEYBOARD_TWO_GOOD_CYNOLOGIST:
+                message = new SendMessage(chatId, "заглушка");
+                break;
+            case KEYBOARD_TWO_NOT_DOG:
+                message = new SendMessage(chatId, "заглушка");
+                break;
+            default:
+                message = new SendMessage(chatId, SORRY_MESSAGE);
+        }
+        return message;
+    }
+
+    private void saveReport(Update update) {
+
+    }
+
 
 }
